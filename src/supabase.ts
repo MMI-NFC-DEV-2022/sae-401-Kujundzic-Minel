71c3ef333c2d9d0c9d7992fc05a1cd8ab5c5edbf
Minel <Minel@example.com>
2024-03-19 10:05:05 +0100

    ajout supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,15 +1,22 @@
 import { createClient } from '@supabase/supabase-js';
+import { ref } from 'vue';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+export const user = ref(null); // Initialiser avec null
 
-console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
+// Ceci est mieux adapté pour être appelé dans un composant ou au démarrage de votre application.
+export async function checkUser() {
+  const { data, error } = await supabase.auth.getUser();
+  if (data) {
+    user.value = data.user;
+  }
+  // Vous pouvez gérer 'error' si nécessaire
+}
 
-// // Identification user
-// const { data } = await supabase.auth.getUser()
-// export const user = ref(data.user)
-// supabase.auth.onAuthStateChange( (evt,session)=>{
-//     user.value = session?.user ?? null
-// })
+// Écouter les changements d'état d'authentification
+supabase.auth.onAuthStateChange((event, session) => {
+  user.value = session?.user ?? null;
+});
