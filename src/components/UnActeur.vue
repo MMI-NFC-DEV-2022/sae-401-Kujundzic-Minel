5b2301fc48c8af812b747cc7fa271782276883a8
Minel <Minel@example.com>
2024-03-20 12:33:50 +0100

    optimisation des codes

--- src/components/UnActeur.vue
+++ src/components/UnActeur.vue
@@ -1,42 +1,26 @@
 <script setup lang="ts">
 import { supabase } from '@/supabase'
 import { useRoute } from 'vue-router'
+
 const route = useRoute()
 const { data: ActeurData } = await supabase
   .from('acteurs')
-  .select('*')
+  .select('*, acteurs_films(films(id, film_name, film_cover))')
   .eq('id', route.params.id)
   .single()
-
-const { data: FilmsActeurs } = await supabase
-  .from('acteurs_films')
-  .select('films(id, film_name, film_cover)')
-  .eq('id_acteur', route.params.id)
 </script>
 
 <template>
-  <div>
-    <div class=" justify-between w-full pb-5">
-      <img
-        :src="ActeurData!.actor_img"
-        alt="Cover image"
-        class="rounded-lg h-80 w-80 object-cover object-top my-4"
-      />
-      <h1 class="text-black text-xl">
-        {{ ActeurData!.actor_name }} {{ ActeurData!.actor_first_name }}
-      </h1>
-    </div>
+  <div class="w-full pb-5">
+    <img :src="ActeurData?.actor_img" alt="Image de l'acteur" class="my-4 rounded-lg h-80 w-80 object-cover object-top" />
+    <h1 class="text-xl text-black">{{ ActeurData?.actor_name }} {{ ActeurData?.actor_first_name }}</h1>
   </div>
 
-  <h2>Les films ou il a joué :</h2>
-  <div v-for="filmacteur in FilmsActeurs" :key="filmacteur.films.id" class="my-4">
-    <router-link :to="`/films/${filmacteur.films.id}`">
-      <img
-        :src="filmacteur.films.film_cover"
-        alt="Cover image"
-        class="rounded-lg h-40 w-40 object-cover object-top mt-4"
-      />
-      <h1 class="text-[#272727] text-xl">{{ filmacteur.films.film_name }}</h1>
+  <h2>Les films où il a joué :</h2>
+  <div v-for="film in ActeurData?.acteurs_films" :key="film.films.id" class="my-4">
+    <router-link :to="`/films/${film.films.id}`">
+      <img :src="film.films.film_cover" alt="Cover image" class="rounded-lg h-40 w-40 object-cover object-top mt-4" />
+      <h1 class="text-xl text-[#272727]">{{ film.films.film_name }}</h1>
     </router-link>
   </div>
 </template>
--- src/components/UnFilm.vue
+++ src/components/UnFilm.vue
@@ -4,23 +4,16 @@ import { useRoute } from 'vue-router'
 const route = useRoute()
 const { data: FilmsData } = await supabase
   .from('films')
-  .select('*')
+  .select(`
+    *,
+    support_film(*, support(*)),
+    physique_film(*, physique(*)),
+    acteurs_films(*, acteurs(*))
+  `)
   .eq('id', route.params.id)
   .single()
-const { data: FilmsSupports } = await supabase
-  .from('support_film')
-  .select('id_support,support(support_name)')
-  .eq('id_film', route.params.id)
-const { data: FilmsActeurs } = await supabase
-  .from('acteurs_films')
-  .select('id_acteur,acteurs(actor_name, actor_first_name, actor_img)')
-  .eq('id_film', route.params.id)
-
-const { data: FilmsPhysiques } = await supabase
-  .from('physique_film')
-  .select('id_physique, physique(physique_name, physique_img)')
-  .eq('id_film', route.params.id)
 </script>
+
 <template>
   <div
     :style="{
@@ -49,55 +42,49 @@ const { data: FilmsPhysiques } = await supabase
       </div>
     </div>
   </div>
-  <div class="m-4">
+  <div class="m-5">
     <h2 class="font-bold text-xl">Synopsis</h2>
     <p>{{ FilmsData!.film_synopsis }}</p>
   </div>
-
-  <div class="text-[#272727] m-4">
-    <h2 class="font-bold text-xl">Ou regarder en streaming ?</h2>
-    <div v-for="Unsupport in FilmsSupports">
-      <p>{{ Unsupport.support.support_name }}</p>
+  <div class="m-5">
+    <h2 class="font-bold text-xl">Où regarder en streaming ?</h2>
+    <div class="flex flex-wrap">
+      <div v-for="supportFilm in FilmsData.support_film" :key="supportFilm.id" class="p-5">
+        <img :src="supportFilm.support.support_img" alt="Image du support" class="rounded-lg w-40">
+        <p>{{ supportFilm.support.support_name }}</p>
+      </div>
     </div>
   </div>
   <div class="flex justify-center">
-    <button class="bg-[#F5C754] text-[#272727] font-bold py-3 w-3/4 rounded-lg">
+    <button class="bg-[#F5C754] text-[#272727] font-bold py-3 w-3/4 rounded-lg mb-5">
       Voir toutes les offres
     </button>
   </div>
-  <h2 class="text-xl font-bold">Support Physique :</h2>
-  <div class="bg-[#272727]">
-    <div class="bg-[#272727] w-full text-white flex">
-      <div
-        v-for="UnPhysique in FilmsPhysiques"
-        :key="UnPhysique.id_physique"
-        class="flex flex-col w-full p-5"
-      >
-        <img
-          :src="UnPhysique.physique!.physique_img"
-          alt=""
-          class="w-full lg:w-1/3 rounded-lg aspect-square object-cover"
-        />
-        <p>{{ UnPhysique.physique.physique_name }}</p>
+  <div class="bg-[#272727] text-white p-5">
+    <div class="bg-[#272727] text-white p-5">
+    <h2 class="text-xl font-bold">Support physique :</h2>
+    <div class="flex flex-wrap">
+      <div v-for="physiqueFilm in FilmsData.physique_film" :key="physiqueFilm.id" class="p-5">
+        <img :src="physiqueFilm.physique.physique_img" alt="Image du support physique" class="rounded-lg w-40">
+        <p>{{ physiqueFilm.physique.physique_name }}</p>
       </div>
     </div>
+  </div>
     <div class="flex justify-center pb-5">
       <button class="bg-[#F5C754] text-[#272727] font-bold py-3 w-3/4 rounded-lg">
         Voir toutes les offres
       </button>
     </div>
   </div>
-  <h2 class="font-bold text-xl pl-5 pt-5">Acteurs</h2>
-  <div v-for="Unacteur in FilmsActeurs" class="w-full p-5">
-    <router-link :to="`/acteurs/${Unacteur.id_acteur}`">
-      <img
-        :src="Unacteur.acteurs.actor_img"
-        alt="Image de l'acteur"
-        class="rounded-lg max-h-44 object-cover object-top mt-4"
-      />
-      <p class="text-[#272727] text-lg font-semibold">
-        {{ Unacteur.acteurs.actor_name }} {{ Unacteur.acteurs.actor_first_name }}
-      </p>
-    </router-link>
+  <div class="m-5">
+    <h2 class="font-bold text-xl">Acteurs</h2>
+    <div class="flex flex-wrap">
+      <div v-for="acteurFilm in FilmsData.acteurs_films" :key="acteurFilm.id" class="p-5">
+        <router-link :to="`/acteurs/${acteurFilm.id_acteur}`">
+          <img :src="acteurFilm.acteurs.actor_img" alt="Image de l'acteur" class="rounded-lg w-40">
+          <p>{{ acteurFilm.acteurs.actor_name }} {{ acteurFilm.acteurs.actor_first_name }}</p>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
